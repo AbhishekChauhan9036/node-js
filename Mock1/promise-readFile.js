17e272e365cb933a1f5921f974a06adf38507760
Abhi <abhishekchauhan9036@gmail.com>
2022-05-10 06:01:26 +0530

    commit message

---
+++ Mock1/promise-readFile.js
@@ -0,0 +1,11 @@
+const fs = require('fs/promises');
+
+async function example() {
+  try {
+    const data = await fs.readFile('/Users/joe/test.txt', { encoding: 'utf8' });
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+example();
