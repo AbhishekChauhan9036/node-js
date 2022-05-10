69bae05e6a1e0dc4719b7c26b6e4e69cd772b2e1
Abhi <abhishekchauhan9036@gmail.com>
2022-05-10 05:59:37 +0530

    commit message

---
+++ Mock1/readFile.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.readFile('/Users/joe/test.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
---
+++ Mock1/readFileSync.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+try {
+  const data = fs.readFileSync('/Users/joe/test.txt', 'utf8');
+  console.log(data);
+} catch (err) {
+  console.error(err);
+}
