8ebf65d4b99d86d010717b7cce504478d15c1f7c
Abhi <abhishekchauhan9036@gmail.com>
2022-05-12 18:39:53 +0530

    Create Server With Node Js

--- Mock1/Create-Node-Js-Server.js
+++ Mock1/Create-Node-Js-Server.js
@@ -3,10 +3,10 @@ const http=require('http');
 const hostname="127.0.0.1";
 
 const port=3000;
-const server=http.createServer((req,res)=>{
-    res.end("Hello World");
+const server=http.createServer((request,responce)=>{
+    responce.end("Namase Node Server");
 });
 
 server.listen(port,hostname,()=>{
-    console.log(`server running ${hostname}:${port}`);
+    console.log(`server running http://${hostname}:${port}`);
 });
